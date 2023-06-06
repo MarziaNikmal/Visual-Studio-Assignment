a1630c0cbfe42083f4579f934176735c09eb39ce
Marzia Nikmal <marzianikmal12@gmail.com>
2023-06-06 00:00:17 -0700

    Writing and Running Code in C# Assignment

---
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+           
+        }
+    }
+}
