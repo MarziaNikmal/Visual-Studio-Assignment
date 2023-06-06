37d8c8267b40264945529eb07764d5e91132595a
Marzia Nikmal <marzianikmal12@gmail.com>
2023-06-06 10:55:37 -0700

    ConsoleApp

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number");
+            string userInput = Console.ReadLine();
+
+            int number = Convert.ToInt32(userInput);
+            int result1 = number * 50;
+            Console.WriteLine("Multiplication Result : " + result1);
+
+            int result2 = number + 25;
+            Console.WriteLine("Addition Result : " + result2);
+
+            double result3 = number / 12.5;
+            Console.WriteLine("Division Result : " + result3);
+
+            bool isGreaterThan50 = number > 50;
+            Console.WriteLine("Is Greater than 50? : " + isGreaterThan50);
+
+            int remainder = number % 7;
+            Console.WriteLine("Remainder : " + remainder);
+
+            Console.ReadLine();
+        }
+    }
+}
