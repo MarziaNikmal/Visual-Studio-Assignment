c91d3178e795c822e95e13986badf85937c4fd1a
Marzia Nikmal <marzianikmal12@gmail.com>
2023-06-12 13:01:01 -0700

    Assignment

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Abstract
+{
+   public abstract class person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void SayName();
+    }
+}
+
+namespace Abstract
+{
+    public class Employee : person
+    {
+        public int id { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("Name :" + FirstName + " " + LastName);
+        }
+    }
+
+}
+
+namespace Abstract
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Employee sam = new Employee() { FirstName = "Sample", LastName = "Student" };
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Define the IQuittable interface with a Quit() method
+public interface IQuittable
+{
+    void Quit();
+}
+
+// Employee class that inherits from IQuittable interface
+public class Employee : IQuittable
+{
+    public string Name { get; set; }
+
+    // Implementation of the Quit() method from the interface
+    public void Quit()
+    {
+        Console.WriteLine("Employee {0} has quit the job.", Name);
+        // Additional logic for quitting the job can be added here
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Create an Employee object
+        Employee employee = new Employee
+        {
+            Name = "John Doe"
+        };
+
+        // Create an object of type IQuittable using polymorphism
+        IQuittable quittable = employee;
+
+        // Call the Quit() method on the IQuittable object
+        quittable.Quit();
+
+        // Output: Employee John Doe has quit the job.
+    }
+}
