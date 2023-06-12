9a0a3ad25ef1dd19439fe5a22f118c4a44497802
Marzia Nikmal <marzianikmal12@gmail.com>
2023-06-11 21:26:56 -0700

    Methods And Objects

---
+++ MethodsandObjects/MethodsandObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodsandObjects
+{
+   public class person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name :" + this.FirstName + " " + this.LastName);
+        }
+    }
+}
+
+namespace MethodsandObjects
+{
+    public class Employee : person
+    {
+        public int Id { get; set; }
+    }
+}
+
+
+
+namespace MethodsandObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sam = new Employee() { FirstName = "Sample", LastName = "Student" };
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
