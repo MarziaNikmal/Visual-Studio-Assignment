53955ffa02697c14318ee47cbee12c7c962dbc76
Marzia Nikmal <marzianikmal12@gmail.com>
2023-06-12 22:38:42 -0700

    Parameters Assignment

---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Employee<T>
+{
+    public List<T> Things { get; set; }
+
+    public Employee(List<T> things)
+    {
+        Things = things;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Instantiate an Employee object with type "string" and assign a list of strings as the property value of Things
+        List<string> stringList = new List<string> { "Apple", "Banana", "Cherry" };
+        Employee<string> stringEmployee = new Employee<string>(stringList);
+
+        // Instantiate an Employee object with type "int" and assign a list of integers as the property value of Things
+        List<int> intList = new List<int> { 1, 2, 3, 4, 5 };
+        Employee<int> intEmployee = new Employee<int>(intList);
+
+        // Create a loop that prints all of the Things to the Console
+        Console.WriteLine("String Things:");
+        foreach (string thing in stringEmployee.Things)
+        {
+            Console.WriteLine(thing);
+        }
+
+        Console.WriteLine("\nInteger Things:");
+        foreach (int thing in intEmployee.Things)
+        {
+            Console.WriteLine(thing);
+        }
+    }
+}
