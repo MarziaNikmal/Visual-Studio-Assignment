d768b3c9cf31a15b3199c176d79fb0be86c593e0
Marzia Nikmal <marzianikmal12@gmail.com>
2023-06-12 23:43:06 -0700

    Struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public struct Number
+{
+    public decimal Amount { get; }
+
+    public Number(decimal amount)
+    {
+        Amount = amount;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Create an object of data type Number and assign an amount to it
+        Number number = new Number(10.5m);
+
+        // Print the amount to the console
+        Console.WriteLine("Amount: " + number.Amount);
+    }
+}
+
